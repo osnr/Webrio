90c82da8161c9b3b03e764f1685dd662c50ba03f
Omar Rizwan <omar@omar.website>
2024-06-20 18:20:40 -0400

    WIP: Use ES6 modules; start on renderer

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 webrio.js: webrio.c ~/aux/libsm64/dist/libsm64.so
 	emcc -sEXPORTED_FUNCTIONS=_webrio_init,_webrio_tick,_webrio_get_sm64_texture_length,_malloc \
 	  	-sEXPORTED_RUNTIME_METHODS=ccall,cwrap \
+		-sEXPORT_ES6=1 -sMODULARIZE -sEXPORT_NAME=webrio \
 		-I ~/aux/libsm64/src -o $@ $^
 
 clean:
--- webrio.c
+++ webrio.c
@@ -2,12 +2,18 @@
 
 #include "webrio.h"
 
+int32_t marioId;
+
 void webrio_init(uint8_t* rom, uint8_t* tex) {
     sm64_global_init(rom, tex);
+
+    /* sm64_static_surfaces_load( surfaces, surfaces_count ); */
+
+    marioId = sm64_mario_create( 0, 1000, 0 );
 }
 
 void webrio_tick() {
-
+    /* sm64_mario_tick(marioId, inputs, outState, outBuffers); */
 }
 
 int webrio_get_sm64_texture_length() {

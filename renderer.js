2543f9c2021fe5b8fd70ec4c93dedec299aa0540
Omar Rizwan <omar@omar.website>
2024-06-20 18:22:21 -0400

    Clear canvas

--- renderer.js
+++ renderer.js
@@ -1,5 +1,16 @@
 export class Renderer {
   constructor(canv) {
-    console.log(canv);
+    this.gl = canv.getContext('webgl');
+    if (this.gl === null) {
+      alert('WebGL initialization failed.');
+    }
+  }
+
+  draw() {
+    const gl = this.gl;
+    // Set clear color to black, fully opaque
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    // Clear the color buffer with specified clear color
+    gl.clear(gl.COLOR_BUFFER_BIT);
   }
 };
